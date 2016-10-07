1d0f04f736195649117b4f49f79aba4e538ea736
IdrisMiles <idris.miles@gmail.com>
2016-10-07 18:17:21 +0100

    initial commit

---
+++ dev/masterclass.pro
@@ -0,0 +1,28 @@
+
+QT       += core
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = masterclass
+DESTDIR = ./bin
+TEMPLATE = app
+
+
+SOURCES += $$PWD/src/*.cpp
+
+HEADERS  += $$PWD/include/*.h
+
+OBJECTS_DIR = ./obj
+MOC_DIR = ./moc
+
+
+# include paths for boost and openvdb
+INCLUDEPATH +=	/home/idris/dev/include \
+		/home/idris/dev/include/bullet \
+		/usr/include/openvdb \
+		./include
+
+# libs required for boost and openvdb
+LIBS +=	-L/home/idris/dev/lib -lBulletDynamics -lBulletCollision -lLinearMath \
+	-L/usr/local/lib -L/usr/lib/x86_64/ -lopenvdb -lpthread -lHalf -ltbb
+
---
+++ dev/src/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+#include <openvdb/openvdb.h>
+
+#include <bullet/btBulletDynamicsCommon.h>
+#include <bullet/BulletCollision/Gimpact/btGImpactCollisionAlgorithm.h>
+
+
+int main(int argc, char** argv)
+{
+    //---------------------------------------------------------------------------------------------------------------------------------
+    // Boilerplate bullet initialisation
+    btBroadphaseInterface *broadPhase = new btDbvtBroadphase();
+
+    btDefaultCollisionConfiguration *collisionConfig = new btDefaultCollisionConfiguration();
+    btCollisionDispatcher *collisionDispatcher = new btCollisionDispatcher(collisionConfig);
+
+    btGImpactCollisionAlgorithm::registerAlgorithm(collisionDispatcher);
+
+    btSequentialImpulseConstraintSolver *solver = new btSequentialImpulseConstraintSolver();
+
+    // initialise the dynamic world
+    btDiscreteDynamicsWorld *dynamicWorld = new btDiscreteDynamicsWorld(collisionDispatcher, broadPhase, solver,collisionConfig);
+    dynamicWorld->setGravity(btVector3(0.0f,-10.0f,0.0f));
+
+
+
+    //---------------------------------------------------------------------------------------------------------------------------------
+    // Setting up dynamic rigidbodies
+
+    // collision shapes
+    btCollisionShape *groundShape = new btStaticPlaneShape(btVector3(0.0f,1.0f,0.0f),1.0f);
+    btCollisionShape *sphereShape = new btSphereShape(1.0f);
+
+    // ground rigid body setup
+    btDefaultMotionState *groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0.0f, 0.0f, 0.0f, 1.0f), btVector3(0.0f, -1.0f, 0.0f)));
+    btRigidBody::btRigidBodyConstructionInfo groundRBCI(0.0f, groundMotionState, groundShape, btVector3(0,0,0));
+    btRigidBody *groundRB = new btRigidBody(groundRBCI);
+    dynamicWorld->addRigidBody(groundRB);
+
+    // sphere rigid body setup
+    btDefaultMotionState *sphereMotionState = new btDefaultMotionState(btTransform(btQuaternion(0.0f, 0.0f, 0.0f, 1.0f), btVector3(0.0f, 50.0f, 0.0f)));
+    btScalar mass = 1;
+    btVector3 sphereInertia = btVector3(0,0,0);
+    sphereShape->calculateLocalInertia(mass,sphereInertia);
+    btRigidBody::btRigidBodyConstructionInfo sphereRBCI(mass, sphereMotionState, sphereShape, sphereInertia);
+    btRigidBody *sphereRB = new btRigidBody(sphereRBCI);
+    dynamicWorld->addRigidBody(sphereRB);
+
+
+
+    //---------------------------------------------------------------------------------------------------------------------------------
+    // OpenVDB setup
+    openvdb::initialize();
+
+    openvdb::FloatGrid::Ptr grid = openvdb::FloatGrid::create();
+
+    std::cout<<"Testing random access:\n";
+
+    openvdb::FloatGrid::Accessor accessor = grid->getAccessor();
+
+    openvdb::Coord coord(10000, -20000000, 3000000);
+
+    accessor.setValue(coord, 1.0);
+
+    std::cout<<"Grid "<< coord <<" = "<< accessor.getValue(coord)<<std::endl;
+
+
+
+
+    //---------------------------------------------------------------------------------------------------------------------------------
+    // Do cool stuff here
+
+    for(int i=0; i<300; i++)
+    {
+        dynamicWorld->stepSimulation(1/60.0f, 10);
+        btTransform sphereTrans;
+        sphereRB->getMotionState()->getWorldTransform(sphereTrans);
+
+        std::cout<<"Sphere Y position:\t"<<sphereTrans.getOrigin().y()<<"\n";
+    }
+
+
+    //---------------------------------------------------------------------------------------------------------------------------------
+
+    dynamicWorld->removeRigidBody(sphereRB);
+    dynamicWorld->removeRigidBody(groundRB);
+
+
+    // free memory
+    delete sphereRB;
+    delete sphereMotionState;
+    delete groundRB;
+    delete groundMotionState;
+    delete groundShape;
+    delete sphereShape;
+
+    delete dynamicWorld;
+    delete solver;
+    delete collisionDispatcher;
+    delete collisionConfig;
+    delete broadPhase;
+
+
+    return 0;
+}
